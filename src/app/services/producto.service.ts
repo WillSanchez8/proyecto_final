0ca532b8544a767f7bc72f5c653b955fa21e000e
FernandoRV98 <jose_rv@tesch.edu.mx>
2023-06-20 19:10:35 -0600

    creando el observable

---
+++ src/app/services/producto.service.ts
@@ -0,0 +1,8 @@
+//IMPORTACIONES
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable ({
+    providedIn: 'root';
+})
