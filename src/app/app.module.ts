2212f8959ed8d3fa76f63fb538aa2a8ed85c15aa
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-06-20 22:59:50 -0600

    método eliminar listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
-import { ToastrModule } from 'ngx-toastr';
 import { ListarProductoComponent } from './components/listar-producto/listar-producto.component';
 import { HttpClientModule} from '@angular/common/http'
 import { ReactiveFormsModule } from '@angular/forms';
@@ -19,7 +18,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    ToastrModule.forRoot(),
     HttpClientModule,
     BrowserAnimationsModule
   ],
--- src/app/components/listar-producto/listar-producto.component.ts
+++ src/app/components/listar-producto/listar-producto.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-listar-producto',
@@ -10,12 +8,12 @@ import { ProductoService } from 'src/app/services/producto.service';
 export class ListarProductoComponent {
 
 
-  constructor(private _productoService:ProductoService,private toastr:ToastrService) {}
+  
 
   eliminarProducto(id:any){
     this._productoService.eliminarProducto(id).subscribe(data=>{
       this.toastr.error('producto eliminado con exito',id);
-      //this.obtenerProductos();
+      this.obtenerProductos();
     },error=>{
       console.log(error);
     }); 
