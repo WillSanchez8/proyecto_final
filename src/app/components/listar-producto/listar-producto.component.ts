57dee1d9e614b6ec3c5440137dabcb478bde2d1c
WillSanchez8 <william_as@tesch.edu.mx>
2023-06-22 23:07:47 -0600

    ya quedo completo el componente listar productos

--- src/app/components/listar-producto/listar-producto.component.ts
+++ src/app/components/listar-producto/listar-producto.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../../services/producto.service';
+import { Producto } from '../../models/producto';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-listar-producto',
   templateUrl: './listar-producto.component.html',
-  styleUrls: ['./listar-producto.component.scss']
+  styleUrls: ['./listar-producto.component.scss'],
 })
-export class ListarProductoComponent {
+export class ListarProductoComponent implements OnInit {
+  listProductos: Producto[] = [];
 
+  constructor(private _productoService:ProductoService, private toastr:ToastrService){}
 
+  ngOnInit(): void {
+    this.obtenerProductos();
+  }
 
+  obtenerProductos() {
+    this._productoService.getProductos().subscribe(
+      (data) => {
+        console.log(data);
+        this.listProductos = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
-  /*eliminarProducto(id:any){
+  eliminarProducto(id:any){
     this._productoService.eliminarProducto(id).subscribe(data=>{
       this.toastr.error('producto eliminado con exito',id);
       this.obtenerProductos();
     },error=>{
       console.log(error);
     }); 
-  }*/
-  
+  }
 }
